c45234fe271682669c7e263cca0376df6ec59de1
rodrigo.cristo.fuhr <UoSUwtxO3cmJK7xUnCtCKNWUNhn0ko0aD3Sa6y>
2024-04-22 20:22:09 -0300

    removido conteudo da sql

--- src/main/resources/db/migration/UW1.0.75__Alterar_Tabela_Item.sql
+++ src/main/resources/db/migration/UW1.0.75__Alterar_Tabela_Item.sql
@@ -1,2 +1,1 @@
-alter table item add column grupo_tributacao_id bigint;
-alter table item add CONSTRAINT item_grupo_tributacao_id_foreign FOREIGN KEY (grupo_tributacao_id) REFERENCES grupo_tributacao (id);
+--removido apos merge
